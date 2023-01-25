2141641a20df68679ee97dff4c46662fe4d8abe4
Nicolas Pucci <nicolaspucci1989@gmail.com>
2023-01-24 23:45:23 -0300

    wip

--- class_02/class_02.js
+++ class_02/class_02.js
@@ -21,6 +21,7 @@ const funLength = function (a, b, c) {}
 console.log(funNoLength.length) // 0
 console.log(funLength.length) // 3
 
+// String conversion
 console.log(String(null)) // 'null'
 console.log(String(undefined)) // 'undefined'
 console.log(String(true)) // 'true'
@@ -29,3 +30,29 @@ console.log(String(1337)) // '1337'
 console.log(String({})) // '[object Object]'
 console.log(String([])) // ''
 console.log(String([1,2,3])) // '1,2,3'
+
+// Number conversion
+console.log(Number(null)) // 0
+console.log(Number(undefined)) // NaN
+console.log(Number(true)) // 1
+console.log(Number(false)) // 0
+console.log(Number({})) // NaN
+console.log(Number([])) // 0
+console.log(Number([1337])) // 1337
+console.log(Number([1,2])) // NaN
+console.log(Number('123')) // 123
+console.log(Number('test')) // NaN
+
+// Boolean conversion
+console.log(Boolean(null)) // false
+console.log(Boolean(undefined)) // false
+console.log(Boolean(NaN)) // false
+console.log(Boolean(0)) // false
+console.log(Boolean('')) // false
+console.log(Boolean('')) // false
+console.log(Boolean(1)) // true
+console.log(Boolean(-1)) // true
+console.log(Boolean([])) // true
+console.log(Boolean(() => {})) // true
+console.log(Boolean('test')) // true
+console.log(Boolean('0')) // true
