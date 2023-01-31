0986402afb537fb42c700f9486c24ef660a5829e
Nicolas Pucci <nicolaspucci1989@gmail.com>
2023-01-30 21:28:41 -0300

    fix sum

--- homework_01/homework-01.js
+++ homework_01/homework-01.js
@@ -1,10 +1,23 @@
 // setup prototype
-String.prototype.plus = function (s) {
-  return (~~this + ~~s).toString()
+String.prototype.plus = function (str) {
+  let ind1 = this.length - 1
+  let ind2 = str.length - 1
+  let res = ""
+  let carry = 0
+  while (ind1 >= 0 || ind2 >= 0 || carry) {
+    const val1 = this[ind1] || 0
+    const val2 = str[ind2] || 0
+    const sum = +val1 + +val2 + carry
+    carry = sum > 9 ? 1 : 0
+    res = sum % 10 + res
+    ind1--
+    ind2--
+  }
+  return res
 }
 
 String.prototype.minus = function (s) {
-  return  (~~this - ~~s).toString()
+  return (~~this - ~~s).toString()
 }
 
 String.prototype.divide = function (s) {
